5227ebffd53df1fd9f5c6227dbc02daf0980d510
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-15 23:47:15 +0200

    Update index.js
    Fix typos

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-import SubHeading from './SubHeading/SubHeading';
+import SubHeading from './SubHeading/Subheading';
 import Newsletter from './Footer/Newsletter';
 import FooterOverlay from './Footer/FooterOverlay';
-import MenuItem from './Menuitem/MenuItem';
+import MenuItem from './Menuitem/Menuitem';
 import Navbar from './Navbar/Navbar';
 
 export {
