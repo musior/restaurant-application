dbc2a8f6285d14da58f446b32a1389e811abaea0
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-17 23:54:47 +0200

    Update Navbar.jsx
    Use react hook (useState) to handle display different navigation for different devices.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 import images from "../../constants/images";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
@@ -37,31 +39,42 @@ const Navbar = () => {
         </a>
       </div>
       <div className="app__navbar-smallscreen">
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
-        <div className="app__navbar-smallscreen_overlay flex__center slider-bottom">
-          <MdOutlineRestaurantMenu
-            fontSize={27}
-            className="overlay__close"
-            onClick={() => {}}
-          />
-          <ul className="app__navbar-smallscreen-links">
-            <li className="p__opensans">
-              <a href="#home">Home</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#about">About</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#menu">Menu</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#awards">Awards</a>
-            </li>
-            <li className="p__opensans">
-              <a href="#contact">Contact</a>
-            </li>
-          </ul>
-        </div>
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => {
+            setToggleMenu(true);
+          }}
+        />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slider-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => {
+                setToggleMenu(false);
+              }}
+            />
+            <ul className="app__navbar-smallscreen-links">
+              <li className="p__opensans">
+                <a href="#home">Home</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#about">About</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#awards">Awards</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contact">Contact</a>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </nav>
   );
