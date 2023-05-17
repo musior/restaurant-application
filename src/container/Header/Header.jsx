d4af0334064fe0643c5ec663669ea84ecb33dc3e
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-18 01:53:36 +0200

    Update Header.jsx
    Create structure of header component.

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import images from "../../constants/images";
+import "./Header.css";
+import Subheading from "../../components/SubHeading/Subheading";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <Subheading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+    </div>
+
+    <div className="app__wrapper_img"></div>
   </div>
 );
 
