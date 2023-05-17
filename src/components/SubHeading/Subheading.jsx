564e4e9130081600a1e8d277d3ec02c469bcb65e
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-18 01:54:05 +0200

    Update Subheading.jsx
    Create structure for subheading component.

--- src/components/SubHeading/Subheading.jsx
+++ src/components/SubHeading/Subheading.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Subheading = () => {
-  return <div>Subheading</div>;
+import { images } from "../../constants";
+
+const Subheading = ({ title }) => {
+  return (
+    <div style={{ marginBottom: "1rem" }}>
+      <p className="p__cormorant">{title}</p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
+    </div>
+  );
 };
 
 export default Subheading;
