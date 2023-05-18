60f8b57cc269de74ad3420261b0e8e51122689a4
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-18 02:04:03 +0200

    Update Header.jsx
    Fix typo in line 23
    Add paragraph and button to the header info

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -9,9 +9,20 @@ const Header = () => (
     <div className="app__wrapper_info">
       <Subheading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+        laborum obcaecati labore ipsam ut odio nostrum optio magnam ducimus in
+        explicabo architecto fuga laudantium, iusto voluptate hic aliquam fugiat
+        quibusdam.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
     </div>
 
-    <div className="app__wrapper_img"></div>
+    <div className="app__header-img">
+      <img src={images.welcome} alt="header img" />
+    </div>
   </div>
 );
 
