4b36c538e456b349ec698fca314c89ecbaf518f9
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-23 18:24:08 +0200

    Update Chef.jsx
    Create structure for Chef component.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { images } from "../../constants";
+import "./Chef.css";
+import Subheading from "../../components/SubHeading/Subheading";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <Subheading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What we believe in</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem,
+            doloribus!
+          </p>
+        </div>
+        <p className="p__opensans">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dolores
+          soluta, neque velit corrupti dolorem placeat dignissimos consequatur
+          accusamus aspernatur.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
