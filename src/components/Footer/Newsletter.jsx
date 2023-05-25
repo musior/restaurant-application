cb7288a1d54306efc16f65a5a22fc94516d9a0db
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-25 18:27:27 +0200

    Update Newsletter.jsx
    Create a structure for Newsletter component.

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
 import "./Newsletter.css";
+import { SubHeading } from "../../components";
 
 const Newsletter = () => {
-  return <div>Newsletter</div>;
+  return (
+    <div className="app__newsletter">
+      <div className="app__newsletter-heading">
+        <SubHeading title="Newsletter" />
+        <h1 className="headtext__cormorant">Subscribe to Our Newsletter</h1>
+        <p className="p__opensans">And never Miss Latest Updates!</p>
+      </div>
+      <div className="app__newsletter-input flex__center">
+        <input type="email" placeholder="Enter your e-mail address" />
+        <button className="custom__button" type="button">
+          Subscribe
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
