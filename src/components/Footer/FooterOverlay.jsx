94c7aa5c92702b7fb05db98462f5fee0526618f2
Mateusz <108770502+musior@users.noreply.github.com>
2023-05-25 18:31:44 +0200

    Update FooterOverlay.jsx
    Create a structure for FooterOverlay component.

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import "./FooterOverlay.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="app__footerOverlay">
+      <div className="app__footerOverlay-black" />
+      <div className="app__footerOverlay-img app__bg" />
+    </div>
+  );
 };
 
 export default Footer;
